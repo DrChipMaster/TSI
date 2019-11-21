de1e3d3c5ed4eb085bfa3a203d35e0e7f58d1168
SimaoAraujo <34342041+SimaoAraujo@users.noreply.github.com>
2019-11-21 19:46:13 +0000

    user profile update -> JAVA

--- Ubiquos/app/src/main/java/com/drchip/ihelp/ui/slideshow/SlideshowFragment.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/ui/slideshow/SlideshowFragment.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.drchip.ihelp.R;
@@ -17,19 +15,39 @@ import com.drchip.ihelp.R;
 public class SlideshowFragment extends Fragment {
 
     private SlideshowViewModel slideshowViewModel;
+    Button btnUserInfo;
+    Button btnUserPublications;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+        btnUserInfo = root.findViewById(R.id.button3);
+        btnUserInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                btnUserInfo.setBackgroundResource(R.drawable.user_profile_on_left);
+                btnUserInfo.setBackgroundResource(R.drawable.user_profile_off_right);
+
+
+                /*    CONTINUAR: MOSTRAR CONTAINER INFO    */
             }
         });
+
+
+        btnUserPublications = root.findViewById(R.id.button2);
+        btnUserPublications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnUserPublications.setBackgroundResource(R.drawable.user_profile_on_right);
+                btnUserPublications.setBackgroundResource(R.drawable.user_profile_off_left);
+
+                /*    CONTINUAR: MOSTRAR CONTAINER INFO    */
+            }
+        });
+
+
         return root;
     }
 }
