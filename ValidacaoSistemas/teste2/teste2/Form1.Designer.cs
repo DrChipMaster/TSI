60bbf4d88a9e63d1f9432941990956fde7fa516e
paulojcpinto <paulojcpinto8@hotmail.com>
2019-11-28 15:10:05 +0000

    O andre é gay

--- ValidacaoSistemas/teste2/teste2/Form1.Designer.cs
+++ ValidacaoSistemas/teste2/teste2/Form1.Designer.cs
@@ -49,6 +49,65 @@
             this.richTextBox1.Size = new System.Drawing.Size(516, 624);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
+<<<<<<< Updated upstream
+=======
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.Yellow;
+            this.checkBox1.Location = new System.Drawing.Point(634, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Check for tabs";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.ForeColor = System.Drawing.Color.Yellow;
+            this.checkBox2.Location = new System.Drawing.Point(634, 36);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(134, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Check for space count";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox3.ForeColor = System.Drawing.Color.Yellow;
+            this.checkBox3.Location = new System.Drawing.Point(634, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(131, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Check for bracket use";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(948, 613);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = false;
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
             // 
             // Form1
             // 
