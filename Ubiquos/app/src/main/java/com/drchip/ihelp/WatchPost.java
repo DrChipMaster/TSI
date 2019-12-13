e75e1b8753dd939b65345e45e5b0c7a611c374fc
SimaoAraujo <34342041+SimaoAraujo@users.noreply.github.com>
2019-12-13 17:10:14 +0000

    watch post: in progress
    MAPS IMPLEMENTED!

--- Ubiquos/app/src/main/java/com/drchip/ihelp/WatchPost.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/WatchPost.java
@@ -4,100 +4,88 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class WatchPost extends AppCompatActivity implements OnMapReadyCallback {
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
-    private MapView mapView;
-    private GoogleMap gMap;
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_watch_post);
 
+        // *** IMPORTANT ***
+        // MapView requires that the Bundle you pass contain _ONLY_ MapView SDK
+        // objects or sub-Bundles.
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
-            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
+        mMapView = findViewById(R.id.mapView);
+        mMapView.onCreate(mapViewBundle);
 
-        mapView = findViewById(R.id.map_View);
-        mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(this);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_user_list, container, false);
-        mUserListRecyclerView = view.findViewById(R.id.user_list_recycler_view);
-        mMapView = view.findViewById(R.id.user_list_map);
-
-        initUserListRecyclerView();
-        initGoogleMap(savedInstanceState);
-
-        return view;
+        mMapView.getMapAsync(this);
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
         if (mapViewBundle == null) {
             mapViewBundle = new Bundle();
-            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
         }
 
-        mapView.onSaveInstanceState(mapViewBundle);
+        mMapView.onSaveInstanceState(mapViewBundle);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mapView.onResume();
+        mMapView.onResume();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mapView.onStart();
+        mMapView.onStart();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        mapView.onStop();
+        mMapView.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        //map.addMarker(new MarkerOptions().position(new LatLng(41.452532, -8.289260)).title("Marker"));
+        map.addMarker(new MarkerOptions().position(new LatLng()))
     }
 
     @Override
     protected void onPause() {
-        mapView.onPause();
+        mMapView.onPause();
         super.onPause();
     }
 
     @Override
     protected void onDestroy() {
-        mapView.onDestroy();
+        mMapView.onDestroy();
         super.onDestroy();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        gMap = googleMap;
-        gMap.setMinZoomPreference(12);
-        LatLng ny = new LatLng(40.7143528, -74.0059731);
-        gMap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+        mMapView.onLowMemory();
     }
 }
 
