30b63e5932318bea6e074ec3fabfed30bb150890
DrChipMaster <andrecampos1998@gmail.com>
2019-12-19 18:38:21 +0000

    user post

--- Ubiquos/app/src/main/java/com/drchip/ihelp/Post.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/Post.java
@@ -3,8 +3,17 @@ package com.drchip.ihelp;
 public class Post {
     public String Author;
     public String Description;
+    public String Title;
     public String ImagePath;
 
+    public Post() {
 
+    }
 
+    public Post(String author, String description, String title, String imagePath) {
+        Author = author;
+        Description = description;
+        Title = title;
+        ImagePath = imagePath;
+    }
 }
