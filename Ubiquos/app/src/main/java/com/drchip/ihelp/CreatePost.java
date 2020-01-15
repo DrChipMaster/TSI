876978617882f26ded1a92007fd782f02d4284b9
DrChipMaster <andrecampos1998@gmail.com>
2020-01-15 14:23:23 +0000

    user post

--- Ubiquos/app/src/main/java/com/drchip/ihelp/CreatePost.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/CreatePost.java
@@ -11,12 +11,24 @@ public class CreatePost extends AppCompatActivity {
 
     EditText etTitle, etDescription;
 
-    ImageView ivAddQRCode;
+    ImageView ivCreatePost, ivCancel, ivAddQRCode, ivAddImage, ivAddLocation, ivAddPhone, ivQRcode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_post);
         ivAddQRCode = findViewById(R.id.ivAddQRCode);
+        etTitle = findViewById(R.id.etTitle);
+        etDescription = findViewById(R.id.etDescription);
+        ivCreatePost = findViewById(R.id.ivCreatePost);
+        ivCancel = findViewById(R.id.ivCancel);
+        ivAddImage = findViewById(R.id.ivAddImage);
+        ivAddLocation = findViewById(R.id.ivAddLocation);
+        ivAddPhone = findViewById(R.id.ivAddPhone);
+        ivQRcode = findViewById(R.id.ivQRcode);
+
+
+
 
         ivAddQRCode.setOnClickListener(new View.OnClickListener() {
             @Override
