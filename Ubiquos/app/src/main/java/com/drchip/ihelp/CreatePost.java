a49363315e572f7a7ccbc0d95506f481a31f8cfd
DrChipMaster <andrecampos1998@gmail.com>
2020-01-15 15:37:57 +0000

    user post

--- Ubiquos/app/src/main/java/com/drchip/ihelp/CreatePost.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/CreatePost.java
@@ -1,17 +1,28 @@
 package com.drchip.ihelp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 public class CreatePost extends AppCompatActivity {
 
     EditText etTitle, etDescription;
 
-    ImageView ivCreatePost, ivCancel, ivAddQRCode, ivAddImage, ivAddLocation, ivAddPhone, ivQRcode;
+    public static final int PICK_IMAGE = 1;
+    ImageView ivCreatePost, ivCancel, ivAddQRCode, ivAddImage, ivAddLocation, ivAddPhone, ivQRcode, ivImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +37,79 @@ public class CreatePost extends AppCompatActivity {
         ivAddLocation = findViewById(R.id.ivAddLocation);
         ivAddPhone = findViewById(R.id.ivAddPhone);
         ivQRcode = findViewById(R.id.ivQRcode);
+        ivImage = findViewById(R.id.ivImage);
 
+
+        ivAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+
+                startActivityForResult(photoPickerIntent, PICK_IMAGE);
+            }
+        });
+
+        ivAddPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder message = new AlertDialog.Builder(CreatePost.this);
+                LayoutInflater inflater = getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.phone_number, null);
+                final EditText etPhoneNumber = dialogView.findViewById(R.id.etPhoneNumber);
+
+                message.setView(dialogView);
+                message.setTitle("Contact");
+                message.setMessage("Enter your contact here!!");
+
+                message.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // etMessage.setText(etReleaseMessage.getText());
+                    }
+                });
+                message.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                message.show();
+
+            }
+        });
+
+
+        ivAddLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder message = new AlertDialog.Builder(CreatePost.this);
+                LayoutInflater inflater = getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.adress_input, null);
+                final EditText etAdress = dialogView.findViewById(R.id.etAdrress);
+
+                message.setView(dialogView);
+                message.setTitle("Adrress");
+                message.setMessage("Enter your adress here here!!");
+
+                message.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        // etMessage.setText(etReleaseMessage.getText());
+                    }
+                });
+                message.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                message.show();
+            }
+        });
 
 
 
@@ -36,5 +119,28 @@ public class CreatePost extends AppCompatActivity {
                 new ImageDownloaderTask(ivQRcode).execute("https://api.qrserver.com/v1/create-qr-code/?size=1000x1000&data=" + "pila");
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+            if (data == null) {
+                //Display an error
+                return;
+            }
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                ivImage.setImageBitmap(selectedImage);
+
+                // mDatabase.child("users").child(ApplicationClass.currentUser.getUid()).setValue(new User(ApplicationClass.currentUser.getDisplayName(), ApplicationClass.currentUser.getEmail(), selectedImage));
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //TODO: action
+        }
     }
 }
