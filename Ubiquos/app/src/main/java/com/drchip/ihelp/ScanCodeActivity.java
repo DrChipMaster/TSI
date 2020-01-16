35dcc12c71126acadc01ca6914ebfbf4dace58ef
DrChipMaster <andrecampos1998@gmail.com>
2020-01-16 15:27:40 +0000

    user post

--- Ubiquos/app/src/main/java/com/drchip/ihelp/ScanCodeActivity.java
+++ Ubiquos/app/src/main/java/com/drchip/ihelp/ScanCodeActivity.java
@@ -1,9 +1,17 @@
 package com.drchip.ihelp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -18,11 +26,41 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
         setContentView(ScannerView);
     }
 
+    private DatabaseReference mDatabase;
 
     @Override
     public void handleResult(Result result) {
+
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        Query myTopPostsQuery = mDatabase.child("Posts").child(result.getText());
+        myTopPostsQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                boolean exits = false;
+                ApplicationClass.PostClicked = dataSnapshot.getValue(Post.class);
+                Intent intent = new Intent(ScanCodeActivity.this, WatchPost.class);
+                startActivity(intent);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         //MainActivity.resultTExtView.setText(result.getText());
-    onBackPressed();
+        //onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent intent = new Intent(ScanCodeActivity.this, MainActivity.class);
+        startActivity(intent);
+
     }
 
     @Override
